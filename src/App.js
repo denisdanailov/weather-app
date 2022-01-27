aafccb755550b2387216b4d7991c58d4cd9b7093
Denis Danailov <denis.danailov@gmx.de>
2022-01-27 22:24:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+const api = {
+  key: "408aab490d1822580b40a7499b56eb22",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
-     <h1>Let start</h1>
+    <div className="app">
+     <main>
+       <div className="search-box">
+         <input 
+         type="text"
+         className="search-bar"
+         placeholder="Search..."
+         />
+       </div>
+     </main>
     </div>
   );
 }
